57d684774a06f5d5996f3de59a286f30918514a6
Ciro Mota <ciro.mota@gmail.com>
2023-12-02 15:14:50 -0300

    feat: Added better way to view public IP

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "linode_public_ip" {
   description = "Public IP Address of instance."
   value = {
-    for instance in linode_instance.terraform-github:
+    for instance in linode_instance.terraform-github :
     instance.label => instance.ip_address
   }
-  sensitive   = false
+  sensitive = false
 }
