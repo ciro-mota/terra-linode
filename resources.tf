aed4fac6d276012f807261e882136c5dd7614b44
Ciro Mota <ciro.mota@gmail.com>
2023-12-11 18:15:47 -0300

    feat: Unified base deb and rhel

--- resources.tf
+++ resources.tf
@@ -5,17 +5,19 @@ resource "random_password" "password" {
   special = true
 }
 
-resource "linode_stackscript" "deb" {
-  label       = "Debian base"
-  description = "Install Nginx in Debian base."
+resource "linode_stackscript" "stack" {
+  label       = "Nginx"
+  description = "Install Nginx."
   is_public   = false
   script      = <<EOF
-#!/bin/bash
-bash <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx-deb.sh)
+#!/bin/sh
+bash <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx.sh)
 EOF
   images = ["linode/ubuntu23.10", "linode/ubuntu23.04", "linode/ubuntu22.10",
-    "linode/ubuntu22.04", "linode/ubuntu20.04", "linode/ubuntu18.04",
-  "linode/ubuntu16.04lts", "linode/debian12", "linode/debian11", "linode/debian10"]
+  "linode/ubuntu22.04", "linode/ubuntu20.04", "linode/ubuntu18.04",
+  "linode/ubuntu16.04lts", "linode/debian12", "linode/debian11",
+  "linode/debian10", "linode/almalinux8", "linode/almalinux9",
+  "linode/fedora37", "linode/fedora38", "linode/rocky8", "linode/rocky9"]
 }
 
 resource "linode_stackscript" "apk" {
@@ -24,21 +26,10 @@ resource "linode_stackscript" "apk" {
   is_public   = false
   script      = <<EOF
 #!/bin/sh
-sh <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx-apk.sh)
+sh <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx.sh)
 EOF
   images = ["linode/alpine3.14", "linode/alpine3.15", "linode/alpine3.16",
   "linode/alpine3.17", "linode/alpine3.18"]
-}
-resource "linode_stackscript" "rhel" {
-  label       = "RHEL base"
-  description = "Install Nginx in RHEL base."
-  is_public   = false
-  script      = <<EOF
-#!/bin/bash
-bash <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx-rhel.sh)
-EOF
-  images = ["linode/almalinux8", "linode/almalinux9", "linode/fedora37",
-  "linode/fedora38", "linode/rocky8", "linode/rocky9"]
 }
 
 # Uncomment one of the two resources below if you are going to use remote state or sending files.
