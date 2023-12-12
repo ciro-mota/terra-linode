65be46ccc7925f368b236fa6e09dc0949d12a312
Ciro Mota <ciro.mota@gmail.com>
2023-12-12 16:27:10 -0300

    fix: Changed stackscript shebang and removed deprecated section

--- resources.tf
+++ resources.tf
@@ -10,7 +10,7 @@ resource "linode_stackscript" "stack" {
   description = "Install Nginx."
   is_public   = false
   script      = <<EOF
-#!/bin/sh
+#!/bin/bash
 bash <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx.sh)
 EOF
   images = ["linode/ubuntu23.10", "linode/ubuntu23.04", "linode/ubuntu22.10",
@@ -31,22 +31,3 @@ EOF
   images = ["linode/alpine3.14", "linode/alpine3.15", "linode/alpine3.16",
   "linode/alpine3.17", "linode/alpine3.18"]
 }
-
-# Uncomment one of the two resources below if you are going to use remote state or sending files.
-# resource "null_resource" "object" {
-
-#  provisioner "local-exec" {
-
-#     command = "aws s3 cp terraform.tfstate s3://<your-bucket-name> --endpoint=https://<your-endpoint-region>.linodeobjects.com"
-#   }
-#   depends_on = [linode_instance.terraform-github]
-# }
-
-# resource "null_resource" "object" {
-
-#  provisioner "local-exec" {
-
-#     command = "linode-cli obj put terraform.tfstate --cluster <your-region> <your-bucket-name>"
-#   }
-#   depends_on = [linode_instance.terraform-github]
-# }
