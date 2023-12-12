41726b3ad029c49c5da1e3f05838d38bcd61b288
Ciro Mota <ciro.mota@gmail.com>
2023-12-12 16:26:10 -0300

    feat: Added remote state option

--- main.tf
+++ main.tf
@@ -3,7 +3,20 @@ terraform {
   required_providers {
     linode = {
       source  = "linode/linode"
-      version = "2.5.2"
+      version = "2.10.1"
     }
   }
+
+  # backend "s3" {
+  #   endpoints                   = { s3 = "https://us-east-1.linodeobjects.com" }
+  #   profile                     = "default"
+  #   skip_credentials_validation = true
+  #   skip_requesting_account_id  = true
+  #   skip_region_validation      = true
+  #   skip_s3_checksum            = true
+  #   bucket                      = "your-bucket-name"
+  #   key                         = "state/terraform.tfstate"
+  #   region                      = "us-east-1"
+  # }
+
 }
