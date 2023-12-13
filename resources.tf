19093e5dc82673290b156f686342b974cb25d560
Ciro Mota <ciro.mota@gmail.com>
2023-12-13 14:29:51 -0300

    feat: Stackscript now is unified for supported distributions.

--- resources.tf
+++ resources.tf
@@ -10,24 +10,14 @@ resource "linode_stackscript" "stack" {
   description = "Install Nginx."
   is_public   = false
   script      = <<EOF
-#!/bin/bash
-bash <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx.sh)
+#!/bin/sh
+curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx.sh | sh
 EOF
   images = ["linode/ubuntu23.10", "linode/ubuntu23.04", "linode/ubuntu22.10",
     "linode/ubuntu22.04", "linode/ubuntu20.04", "linode/ubuntu18.04",
     "linode/ubuntu16.04lts", "linode/debian12", "linode/debian11",
     "linode/debian10", "linode/almalinux8", "linode/almalinux9",
-  "linode/fedora37", "linode/fedora38", "linode/rocky8", "linode/rocky9"]
-}
-
-resource "linode_stackscript" "apk" {
-  label       = "Alpine"
-  description = "Install Nginx in Alpine."
-  is_public   = false
-  script      = <<EOF
-#!/bin/sh
-sh <(curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx.sh)
-EOF
-  images = ["linode/alpine3.14", "linode/alpine3.15", "linode/alpine3.16",
+    "linode/fedora37", "linode/fedora38", "linode/rocky8", "linode/rocky9",
+    "linode/alpine3.14", "linode/alpine3.15", "linode/alpine3.16",
   "linode/alpine3.17", "linode/alpine3.18"]
 }
