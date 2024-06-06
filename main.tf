04dbc49d6f8dba89d5f7dadde3a214abfd639e32
Ciro Mota <ciro.mota@gmail.com>
2024-06-06 16:52:24 -0300

    fix: Compatibility with Terraform and OpenTofu versions

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 1.6.2"
+  required_version = ">= 1.7.2"
   required_providers {
     linode = {
       source  = "linode/linode"
-      version = "2.10.1"
+      version = "2.20.1"
     }
   }
 
   # backend "s3" {
   #   endpoints                   = { s3 = "https://us-east-1.linodeobjects.com" }
-  #   profile                     = "default"
+  #   profile                     = "linode"
   #   skip_credentials_validation = true
   #   skip_requesting_account_id  = true
   #   skip_region_validation      = true
