436ad664ff1b447c2cabfc9b1f00333de684dbc5
Ciro Mota <ciro.mota@gmail.com>
2025-02-20 16:07:42 -0300

    fix: Limited stackscript to only stable versions

--- resources.tf
+++ resources.tf
@@ -7,17 +7,14 @@ resource "random_password" "password" {
 
 resource "linode_stackscript" "stack" {
   label       = "Nginx"
-  description = "Install Nginx."
+  description = "Install Nginx. Only in stable versions."
   is_public   = false
   script      = <<EOF
 #!/bin/sh
-curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/scripts/nginx.sh | sh
+curl -sk https://raw.githubusercontent.com/ciro-mota/terra-linode/main/files/nginx.sh | sh
 EOF
-  images = ["linode/ubuntu23.10", "linode/ubuntu23.04", "linode/ubuntu22.10",
-    "linode/ubuntu22.04", "linode/ubuntu20.04", "linode/ubuntu18.04",
-    "linode/ubuntu16.04lts", "linode/debian12", "linode/debian11",
-    "linode/debian10", "linode/almalinux8", "linode/almalinux9",
-    "linode/fedora37", "linode/fedora38", "linode/rocky8", "linode/rocky9",
-    "linode/alpine3.14", "linode/alpine3.15", "linode/alpine3.16",
-  "linode/alpine3.17", "linode/alpine3.18"]
+  images = ["linode/ubuntu24.04", "linode/ubuntu22.04",
+    "linode/ubuntu20.04", "linode/debian12",
+    "linode/almalinux8", "linode/almalinux9",
+  "linode/rocky8", "linode/rocky9"]
 }
