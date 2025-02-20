ea0cc946efb6cf7f04ce299249f2496d5de85fe5
Ciro Mota <ciro.mota@gmail.com>
2025-02-20 15:57:29 -0300

    fix: Update provider and required version to reflect Terraform and Tofu compability

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.8.1"
+  required_version = "1.9.0"
   required_providers {
     linode = {
       source  = "linode/linode"
-      version = "2.25.0"
+      version = "2.34.1"
     }
   }
 
